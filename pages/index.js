9e893933dbbea2177ff5f695425cd93fe1f6dab3
Matthew Daniel Murphy <matt@mattmurphy.ca>
2020-12-01 16:48:19 -0700

    change index heading

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,18 @@
-import Layout from "../components/layout";
+import Layout from "../components/layout"
 
 function IndexPage() {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center">
-        <img
-          src="team-of-critters.svg"
-          className="w-full max-w-xl"
-          alt="Four one-eyed aliens playing"
-        />
-
-        <h2 className="p-3 my-8 text-lg font-bold bg-yellow-400 md:text-2xl">
-          Hi! Welcome to your first Next.js site.
+        <h2 className="p-5 my-8 text-lg font-bold bg-pink-200 md:text-2xl">
+          Welcome to Canopia.ca
         </h2>
       </div>
+      <div className="flex flex-col items-center justify-center">
+        <h3 className="p-4 my-4 text-md font-bold md:text-1xl">Latest posts</h3>
+      </div>
     </Layout>
-  );
+  )
 }
 
-export default IndexPage;
+export default IndexPage
