9e72dc6f70c8d6948f6f7ed8dc35948dc47aa6f8
Matthew Daniel Murphy <matt@mattmurphy.ca>
2020-12-02 10:26:35 -0700

    add dynamic routing & dynamic list of blog posts to index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "../components/layout"
+import { getSortedPostsData } from "../lib/posts"
 
-function IndexPage() {
+function IndexPage({ allPostsData }) {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center">
@@ -10,9 +11,21 @@ function IndexPage() {
       </div>
       <div className="flex flex-col items-center justify-center">
         <h3 className="p-4 my-4 text-md font-bold md:text-1xl">Latest posts</h3>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              <a href={`/posts/${id}`}>{title}</a> ({date})
+            </li>
+          ))}
+        </ul>
       </div>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return { props: { allPostsData } }
 }
 
 export default IndexPage
