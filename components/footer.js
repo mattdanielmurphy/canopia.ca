811037df6891f61b8c0f92b7426d9f009dec13c4
Matthew Daniel Murphy <matt@mattmurphy.ca>
2020-12-07 16:52:22 -0700

    update footer component

--- components/footer.js
+++ components/footer.js
@@ -1,21 +1,11 @@
 function Footer() {
   return (
-    <footer className="bg-blue-500">
+    <footer className="bg-black">
       <ul className="flex items-center justify-between max-w-4xl p-4 mx-auto text-sm text-white md:p-8">
         <li>
           Created by{" "}
-          <a href="https://bryant.io" target="_blank" className="font-bold">
-            Taylor Bryant
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="https://github.com/oddstronaut/tailwind-next"
-            target="_blank"
-            className="font-bold"
-          >
-            GitHub
+          <a href="https://www.linkedin.com/in/matthew-daniel-m-00b38489/">
+            Matthew Daniel Murphy
           </a>
         </li>
       </ul>
