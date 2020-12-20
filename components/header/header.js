bd82b2e54e340eb33bec1e37378788e64eaa1074
Matthew Daniel Murphy <matt@mattmurphy.ca>
2020-12-20 00:26:12 -0700

    add mobile nav functionality

--- components/header/header.js
+++ components/header/header.js
@@ -1,43 +1,71 @@
 import Link from "next/link"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import styled from "styled-components"
+import useWindowSize from "../../lib/useWindowSize"
 
-function Header({ borderless, floatOnTop }) {
-  const [isExpanded, toggleExpansion] = useState(false)
+const Button = styled.button`
+  width: 40px;
+  appearance: none;
+  background: none;
+  border: none;
+`
+
+const NavList = styled.ul`
+  ${({ mobileView, isExpanded }) =>
+    mobileView && !isExpanded && "display: none"}
+`
+
+function Header() {
+  const [isExpanded, setExpansion] = useState(false)
+  const toggleExpansion = () => setExpansion(!isExpanded)
+  const [mobileView, setMobileView] = useState(true)
+  const windowSize = useWindowSize()
+
+  useEffect(() => {
+    const mobileWidth = 450
+    if (windowSize.width <= mobileWidth) {
+      if (!mobileView) {
+        setExpansion(false) // so doesn't collapse again if made smaller
+        setMobileView(true)
+      }
+    } else setMobileView(false)
+    // desired behavior: collapse if set to mobile width, but don't recollapse if already in mobile view
+  }, [windowSize.width])
 
   return (
     <header>
       <div>
-        <div>
-          <Link href="/">
-            <a>
-              Canopia<span style={{ fontWeight: "normal" }}>.ca</span>
-            </a>
-          </Link>
-        </div>
+        <Link href="/">
+          <a>
+            <strong>Canopia</strong>.ca
+          </a>
+        </Link>
+      </div>
 
-        <button onClick={() => toggleExpansion(!isExpanded)}>
+      {mobileView && (
+        <Button onClick={toggleExpansion}>
           <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
-        </button>
+        </Button>
+      )}
 
-        <ul>
-          {[
-            { title: "About", route: "/about" },
-            { title: "Contact", route: "/contact" },
-            { title: "Presentation", route: "/presentation" },
-            { title: "Articles & Resources", route: "/articles-and-resources" },
-            { title: "Blog Posts", route: "/blog-posts" },
-          ].map((navigationItem) => (
-            <li key={navigationItem.title}>
-              <Link href={navigationItem.route}>
-                <a>{navigationItem.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <NavList mobileView={mobileView} isExpanded={isExpanded}>
+        {[
+          { title: "About", route: "/about" },
+          { title: "Contact", route: "/contact" },
+          { title: "Presentation", route: "/presentation" },
+          { title: "Articles & Resources", route: "/articles-and-resources" },
+          { title: "Blog Posts", route: "/blog-posts" },
+        ].map((navigationItem) => (
+          <li key={navigationItem.title}>
+            <Link href={navigationItem.route}>
+              <a>{navigationItem.title}</a>
+            </Link>
+          </li>
+        ))}
+      </NavList>
     </header>
   )
 }
