1813a17a33f37b74fb0be7e94daa1db524ebf0cd
Matthew Daniel Murphy <matt@mattmurphy.ca>
2021-01-02 04:47:06 -0700

    load smooth scroll polyfill if needed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import 'normalize.css'
 import '../styles/app.css'
 
+function loadScript(src) {
+  const js = document.createElement('script')
+  js.src = src
+  js.onerror = () => error.log('Failed to load script ' + src)
+  document.head.appendChild(js)
+}
+
+if (process.browser) {
+  if (getComputedStyle(document.body).scrollBehavior !== 'smooth') {
+    loadScript(
+      'https://cdnjs.cloudflare.com/ajax/libs/iamdustan-smoothscroll/0.4.0/smoothscroll.min.js',
+    )
+  }
+}
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
